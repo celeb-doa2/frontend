98c914af3b0b3f1ef9f1bfb8b41c06fd684b5059
Kristi Gribble <kgribble4083@gmail.com>
2020-01-06 23:39:34 -0600

    fixing errors

--- celeb-doa2/src/App.js
+++ celeb-doa2/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import SignUp from "../src/components/SignUp";
 import Button from "../src/components/Button";
 import SignIn from "../src/components/SignIn";
@@ -15,30 +14,5 @@ function App() {
       
     </main>
   )
-=======
-import Button from "./components/Button";
-
-function App() {
-  return (
-    <div className="App">
-      <Button />
-    </div>
-  );
->>>>>>> bd2b638ba17ac37193e4d10e5719c59517321b16
-=======
-
-import SignUp from "../src/components/SignUp";
-
-function App() {
-    
-  return (
-   <main> 
-      <SignUp /> {/*this is temporary rendering, to make sure it looks as it should*/}
-      </main>
-  )
-
->>>>>>> 56cb4c4cba6d7543eb089ad47b1129aa5068a94b
 }
-
-
-export default App;
+  export default App;
