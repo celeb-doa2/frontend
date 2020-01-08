8c6ce4d6e2cc9387758f65ee23150cb22d5fc4fb
AJ Brush <19802883+ajb85@users.noreply.github.com>
2020-01-07 22:39:58 -0500

    removes token

--- celeb-doa2/src/App.js
+++ celeb-doa2/src/App.js
@@ -1,18 +1,16 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import './App.css';
 
-import SignUp from "../src/components/SignUp";
-import Button from "../src/components/Button";
-import SignIn from "../src/components/SignIn";
+import SignUp from '../src/components/Signup';
+import Button from '../src/components/Button';
+import SignIn from '../src/components/Signin';
 
 function App() {
-    
   return (
-    <main> 
-      <SignUp /> {/*this is temporary rendering, to make sure it looks as it should*/}
-      
-      
+    <main>
+      <SignUp />
+      {/*this is temporary rendering, to make sure it looks as it should*/}
     </main>
-  )
+  );
 }
-  export default App;
+export default App;
