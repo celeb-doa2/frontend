4687eff3cd06bb902c4487568204f3678bbb5b1c
Kristi Gribble <kgribble4083@gmail.com>
2020-01-08 19:49:48 -0600

    updated formatting

--- celeb-doa2/src/components/CelebCard.js
+++ celeb-doa2/src/components/CelebCard.js
@@ -12,42 +12,42 @@ const CelebCard = () => {
         padding: 2%;
         background-color: #7F74FF;
         border-radius: 10px;
-    margin: 2%;
-    align-items: center;
-    box-shadow: -5px 5px 15px #3A3574;
+        margin: 2%;
+        align-items: center;
+        box-shadow: -5px 5px 15px #3A3574;
     `;
 
    const Logo = styled.a`
-   display: flex;
-   flex-direction: row;
-   position: relative;
-   margin-left: auto;
-   margin-right: auto;
-   width: 50%;      
+        display: flex;
+        flex-direction: row;
+        position: relative;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%;      
    `;
 
    const Tagline = styled.h3`
-   font-size: 1.5em;
-   text-align: center;
-   line-height: 1;
-   paddding: 0%;
-   font-family: 'Roboto', sans-serif;
-   font-weight: 500;
-   color: #18E7DD;
-   text-shadow: -3px 3px #3A3574;
-   -webkit-text-stroke: 0.5px #03E490;    
+        font-size: 1.5em;
+        text-align: center;
+        line-height: 1;
+        paddding: 0%;
+        font-family: 'Roboto', sans-serif;
+        font-weight: 500;
+        color: #18E7DD;
+        text-shadow: -3px 3px #3A3574;
+        -webkit-text-stroke: 0.5px #03E490;    
    `;
 
    const Name = styled.p`
-   font-size: 1.5em;
-   text-align: center;
-   line-height: 1;
-   paddding: 0%;
-   font-family: 'Roboto', sans-serif;
-   font-weight: 500;
-   color: #18E7DD;
-   text-shadow: -3px 3px #3A3574;
-   -webkit-text-stroke: 0.5px #03E490;    
+        font-size: 1.5em;
+        text-align: center;
+        line-height: 1;
+        paddding: 0%;
+        font-family: 'Roboto', sans-serif;
+        font-weight: 500;
+        color: #18E7DD;
+        text-shadow: -3px 3px #3A3574;
+        -webkit-text-stroke: 0.5px #03E490;    
    `;
 
     const [celeb, setCeleb] = useState([]);
@@ -66,12 +66,12 @@ const CelebCard = () => {
     return (
         <FormBox>
         <form>
-        <Logo><a href="https://doa2.netlify.com/"><img src="https://i.imgur.com/Kc4PN2y.png"></img></a></Logo>
-        <Tagline><h3>...(not)quite dead yet?</h3></Tagline>
-        <Name><p>{celeb.name}</p></Name>
-        <img className="image" src={celeb.image_url}></img>
-        <p>Born: {celeb.birth} - ???</p>
-        <p><Button label="ALIVE"></Button> OR <Button label="DEAD"></Button></p>
+            <Logo><a href="https://doa2.netlify.com/"><img src="https://i.imgur.com/Kc4PN2y.png"></img></a></Logo>
+                <Tagline><h3>...(not)quite dead yet?</h3></Tagline>
+                <Name><p>{celeb.name}</p></Name>
+                <img className="image" src={celeb.image_url}></img>
+                <p>Born: {celeb.birth} - ???</p>
+                <p><Button label="ALIVE"></Button> OR <Button label="DEAD"></Button></p>
 
         </form>
         </FormBox>
