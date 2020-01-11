472b08e8bf6b2d773b21eabfdb00b82b965a461e
Kristi Gribble <kgribble4083@gmail.com>
2020-01-10 21:03:27 -0600

    Fixed formatting

--- celeb-doa2/src/components/SignIn.js
+++ celeb-doa2/src/components/SignIn.js
@@ -12,43 +12,43 @@ const SignIn = () => {
     `;
 
     const FormBox = styled.form`
-     width: 437px;
-     height: 320px;
-     padding: 2%;
-     background-color: #7F74FF;
-     border-radius: 10px;
-     margin: 2%;
-     align-items: center;
-     box-shadow: -5px 5px 15px #3A3574;
-     `;
+        width: 437px;
+        height: 320px;
+        padding: 2%;
+        background-color: #7F74FF;
+        border-radius: 10px;
+        margin: 2%;
+        align-items: center;
+        box-shadow: -5px 5px 15px #3A3574;
+    `;
 
     const Logo = styled.a`
-    display: flex;
-    flex-direction: row;
-    position: relative;
-    margin-left: auto;
-    margin-right: auto;
-    width: 50%;      
+        display: flex;
+        flex-direction: row;
+        position: relative;
+        margin-left: auto;
+        margin-right: auto;
+        width: 50%;      
     `;
 
 
     const Tagline = styled.h3`
-    font-size: 1.5em;
-    text-align: center;
-    line-height: 1;
-    padding: 0%;
-    font-family: 'Roboto', sans-serif;
-    font-weight: 500;
-    color: #18E7DD;
-    text-shadow: -3px 3px #3A3574;
-    -webkit-text-stroke: 0.5px #03E490;    
+        font-size: 1.5em;
+        text-align: center;
+        line-height: 1;
+        padding: 0%;
+        font-family: 'Roboto', sans-serif;
+        font-weight: 500;
+        color: #18E7DD;
+        text-shadow: -3px 3px #3A3574;
+        -webkit-text-stroke: 0.5px #03E490;    
     `;
 
     const Input = styled.text`
-    display: flex; 
-    flex-direction: column;
-    padding: 2%;
-    margin: 2.5%;
+        display: flex; 
+        flex-direction: column;
+        padding: 2%;
+        margin: 2.5%;
     `;
 
     const {register, handleSubmit, errors} = useForm();
